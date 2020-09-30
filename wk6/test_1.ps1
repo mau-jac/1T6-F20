84fe841bd2508963ea8e8a41fe0ef57a7487ec50
Mauricio Buschinelli <mauricio.buschinelli@johnabbott.qc.ca>
2020-09-30 12:39:59 -0400

    add test_1.ps1 file to wk 6

---
+++ wk6/test_1.ps1
@@ -0,0 +1,15 @@
+﻿$top = "T1 Files"
+
+# If path exists delete it
+if( Test-Path .\$top){ rm .\$top -Recurse }
+
+mkdir .\$top\Data\Random
+mkdir .\$top\1T6
+
+# PowerShell array
+$txt_names = @('games','weekend','movies')
+
+foreach( $name in $txt_names ){
+    ni .\$top\Data\$name.txt
+    ni .\$top\Data\$name.png
+}
